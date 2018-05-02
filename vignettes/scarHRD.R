70dac9c026f920cd08987d54448a982533db6ad5
sztup <sztup@users.noreply.github.com>
2018-05-02 20:39:22 +0200

    Add files via upload

---
+++ vignettes/scarHRD.R
@@ -0,0 +1,7 @@
+## ----part1, echo=TRUE, eval=FALSE----------------------------------------
+#  library(devtools)
+#  install_github('sztup/scarHRD',build_vignettes = TRUE)
+
+## ----part2, echo=TRUE, eval=FALSE----------------------------------------
+#  getwd()
+
