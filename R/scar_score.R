f319e4ba3d301acd40de35d99946e4f3dd12cd8b
sztup <sztup@users.noreply.github.com>
2020-08-11 18:09:40 -0400

    chromosome nomenclature and mouse genome

--- R/scar_score.R
+++ R/scar_score.R
@@ -1,23 +1,38 @@
 #' Scar score
 #'
-#' Determining genomic scar score (telomeric allelic imbalance, loss of heterozygosity, large-scale transitions), signs of homologous recombination deficiency
+#' Determining genomic scar score (telomeric allelic imbalance, loss-off heterozigosity, large-scle transitions), signs of homologous recombination deficiency
 #' @param seg Imput file: either a binned sequenza out file (or any other segmentation file with the following columns: chromosome, position, base.ref, depth.normal, depth.tumor, depth.ratio, Af, Bf, zygosity.normal, GC.percent, good.reads, AB.normal, AB.tumor, tumor.strand) or an allele-specific segmentation file with the following columns: 1st column: sample name, 2nd column: chromosome, 3rd column: segmentation start, 4th column: segmentation end, 5th column: total copynumber, 6th column: copy number of A allele, 7th column: copy number of B allele
 #' @param reference: reference genome: either grch38 or grch37. Default is grch38.
 #' @param seqz Optional parameter, set to TRUE, if input file is a binned sequenza file.
 #' @param ploidy Optional parameter, may be used if the ploidy of the sample is known.
+#' @param chr.in.names Optional parameter, default: TRUE, set to FALSE if input file does not contain 'chr' in chromosome names.
 #' @return Output is, with the following columns: HRD	Telomeric AI	Mean size	Interstitial AI	Mean Size	Whole chr AI	Telomeric LOH	Mean size	Interstitial LOH	Mean Size	Whole chr LOH	Ploidy	Aberrant cell fraction	LST	HRDscore	adjustedHRDscore
 #' @export
 #' @import sequenza
 #' @import data.table
-scar_score<-function(seg,reference = "grch38", seqz=FALSE, ploidy=NULL, sizelimitLOH=15e6, outputdir=NULL){
+scar_score<-function(seg,reference = "grch38", chr.in.names=TRUE, m,seqz=FALSE, ploidy=NULL, sizelimitLOH=15e6, outputdir=NULL){
 
   if (is.null(outputdir)){
   outputdir=getwd()
   }
   if (reference == "grch38"){
     chrominfo = chrominfo_grch38
+    if(!chr.in.names){
+      chrominfo$chrom = gsub("chr","",chrominfo$chr)
+      rownames(chrominfo) = chrominfo$chr
+    }
   } else if(reference == "grch37"){
     chrominfo = chrominfo_grch37
+    if(!chr.in.names){
+      chrominfo$chrom = gsub("chr","",chrominfo$chr)
+      rownames(chrominfo) = chrominfo$chr
+    }
+  } else if(reference == "mouse"){
+    chrominfo = chrominfo_mouse
+    if(!chr.in.names){
+      chrominfo$chrom = gsub("chr","",chrominfo$chr)
+      rownames(chrominfo) = chrominfo$chr
+    }
   } else {
     stop()
   }
